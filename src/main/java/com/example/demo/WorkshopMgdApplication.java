d93c800c031d5dd7414ef19727e3ff7f722b4128
DowglasMaia <luciodowglas@live.com>
2018-08-08 09:14:56 -0300

    build project

---
+++ src/main/java/com/example/demo/WorkshopMgdApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMgdApplication.class, args);
+	}
+}
